5865e1f92872d05e7c101e48c5bdd3309046096e
Theodore Stites <argionad@gmail.com>
2023-03-10 21:59:23 -0500

    Replace HTML library with Express

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
+var express = require('express');
+
+var app = express();
+app.get('/evesswearjar', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = "Hello Eve's chat! I'm a web app that saves and categorizes Eve's curses :)" ,
-        response = [message].join('\n');
+    var response = "Hello Eve's chat! I'm a web app that saves and categorizes Eve's curses :) : " + req.query.phrase;
     res.end(response);
-});
-server.listen();
+})
+app.listen();
