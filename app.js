537cc8d5a87e351b2eea76275108a8d5c17ff8ae
Theodore Stites <argionad@gmail.com>
2023-03-14 22:27:34 -0400

    Refactor part of app.js into phrase.js

--- app.js
+++ app.js
@@ -1,49 +1,29 @@
 var express = require('express');
 const DBConnection = require('./dbconnection.js');
+const Phrase = require('./phrase.js');
+const Dictionary = require('./dictionary.js');
 
-var dict = [["motherfucker", "fuck"], ["bullshit", "shit"], ["asshole", "ass"], ["damn", "dammit"], ["bitch"], ["tits"], ["dick"]];
 var app = express();
 app.get('/evesswearjar', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+
     var dbConnection = new DBConnection();
+    var dictionary = new Dictionary();
+    var phrase = new Phrase(req.query.phrase);
 
-    var phrase = req.query.phrase;
-    var phraseArray = phrase.split(' ');
-
-    var match = "";
     var wordArray = new Array();
     
     //Search submission for matching words in dict array
-    phraseArray.forEach((phraseWord) => {
-        dict.forEach((category) => {
-            for (var i = 0; i < category.length; i++) {
-                var word = category[i];
-
-                if (phraseWord.match("^.*" + word)) {
-                    match += word + ", ";
-                    wordArray.push(word);
-    
-                    //Once a word in a category has been found, skip the rest of the category
-                    break;
-                }
-            }
-        });
-    
-    });
-    //Delete remaining ", " from text
-    match = match.substring(0, match.length - 2);
+    wordArray = dictionary.getMatchingArray(phrase.getPhraseArray());
 
     //If there is data, connect to DB and insert data
     if (wordArray.length > 0) {
         wordArray.forEach ((word) => {
-            dbConnection.insert("swears", ["category", "phrase"], [word, phrase]);
+            //dbConnection.insert("swears", ["category", "phrase"], [word, phrase.getPhrase()]);
         });
     }
     //If there is a successful connection, note how many times Eve has cursed this stream (maybe her most used word too?)
-    if (match != "")
-        var response = match + " :)";
-    else
-        var response = phrase + " :)"
+    var response = phrase.getPhrase() + " :)"
     res.end(response);
 })
 app.listen();
--- phrase.js
+++ phrase.js
@@ -1,9 +1,19 @@
 class Phrase {
+    #phrase;
+    #phraseSplit;
+
     constructor(phrase) {
-        this.phrase = phrase;
+        this.#phrase = phrase;
+        this.#phraseSplit = this.#phrase.split(' ');
     }
 
-    
+    getPhrase() {
+        return this.#phrase;
+    }
+
+    getPhraseArray() {
+        return this.#phraseSplit;
+    }
 }
 
 module.exports = Phrase;
