2f227b3e3343e59b829ed53c413d027762e66758
Theodore Stites <argionad@gmail.com>
2023-03-14 22:07:10 -0400

    add dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,34 @@
+class Dictionary {
+    #dict;
+
+    constructor() {
+        this.#dict = [["motherfucker", "fuck"], ["bullshit", "shit"], ["asshole", "ass"], ["damn", "dammit"], ["bitch"], ["tits"], ["dick"]];
+    }
+
+    getDictionary() {
+        return this.#dict;
+    }
+
+    getMatchingArray(phraseArray) {
+        var wordArray = new Array();
+
+        phraseArray.forEach((phraseWord) => {
+            this.#dict.forEach((category) => {
+                for (var i = 0; i < category.length; i++) {
+                    var word = category[i];
+    
+                    if (phraseWord.match("^.*" + word)) {
+                        wordArray.push(word);
+        
+                        //Once a word in a category has been found, skip the rest of the category
+                        break;
+                    }
+                }
+            });
+        });
+
+        return wordArray;
+    }
+}
+
+module.exports = Dictionary;
