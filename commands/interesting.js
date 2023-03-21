7c69b27fb39e1b396111f1a4344c5a93f2ca9514
Theodore Stites <argionad@gmail.com>
2023-03-21 02:28:03 -0400

    Add command code to interesting.js

--- commands/interesting.js
+++ commands/interesting.js
@@ -1,9 +1,18 @@
 const Command = require('./command.js');
+const DBConnection = require('../dbconnection.js');
 
 class Interesting extends Command {
     run() {
+        var dbConnection = new DBConnection();
+
         return new Promise((resolve, reject) => {
-            resolve(this.command);
+            dbConnection.update("swears", ["interesting"], ["1"], "id", this.commandArray[1])
+                .then((value) => {
+                    resolve("Noted Kappa");
+                })
+                .catch((err) => {
+                    reject(err);
+                });
         });
     }
 }
