b99905fac20144f445a72ce9d7e196c83b0c851a
Theodore Stites <argionad@gmail.com>
2023-03-25 01:40:48 -0400

    Add some SQL injection resistance to DB methods

--- dbconnection.js
+++ dbconnection.js
@@ -14,8 +14,8 @@ class DBConnection {
 
     queryDB(query) {
         var conn = this.#connect();
-
         conn.connect();
+
         return new Promise((resolve, reject) => {
             conn.query(query, function(err, result) {
                 if (result === undefined) {
@@ -30,32 +30,48 @@ class DBConnection {
     }
 
     select(select, from, where = null, whereCondition = null, orderBy = null, orderByOrder = null) {
-        var query = `SELECT ${select} FROM ${from}`;
+        var inputTester = /^[A-Za-z0-9*]+$/;
 
-        if (where != null && whereCondition != null)
-            query += ` WHERE ${where} = '${whereCondition}'`;
+        if (select.match(inputTester) && from.match(inputTester)) {
+            var query = `SELECT ${select} FROM ${from}`;
 
-        if (orderBy != null && orderByOrder != null)
-            query += ` ORDER BY ${orderBy} ${orderByOrder}`;
+            if (where != null && whereCondition != null && where.match(inputTester) && whereCondition.match(inputTester))
+                query += ` WHERE ${where} = '${whereCondition}'`;
 
-        return this.queryDB(query);
+            if (orderBy != null && orderByOrder != null && orderBy.match(inputTester) && orderByOrder.match(inputTester))
+                query += ` ORDER BY ${orderBy} ${orderByOrder}`;
+
+            return this.queryDB(query);
+        } else {
+            return new Promise((resolve, reject) => {
+                reject(new Error("Invalid command entered"));
+            })
+        }
     }
     
     selectGroup(select, from, group, where = null, whereCondition = null, orderBy = null, limit = false) {
-        var query = 'SELECT ' + select + ' FROM ' + from;
+        var inputTester = /^[A-Za-z0-9*]+$/;
 
-        if (where != null && whereCondition != null)
-            query += ` WHERE ${where} = '${whereCondition}'`
-        
-        query += ' GROUP BY ' + group;
-        
-        if (orderBy != null)
-            query += ' ORDER BY ' + orderBy;
+        if (select.match(inputTester) && from.match(inputTester) && group.match(inputTester)) {
+            var query = `SELECT ${select} FROM ${from}`;
 
-        if (limit)
+            if (where != null && whereCondition != null && where.match(inputTester) && whereCondition.match(inputTester))
+                query += ` WHERE ${where} = '${whereCondition}'`
+            
+            query += ' GROUP BY ' + group;
+            
+            if (orderBy != null && orderBy.match(inputTester))
+                query += ' ORDER BY ' + orderBy + ' DESC';
+
+            if (limit)
                 query += ' LIMIT 1';
 
-        return this.queryDB(query);
+            return this.queryDB(query);
+        } else {
+            return new Promise((resolve, reject) => {
+                reject(new Error("Invalid command entered"));
+            });
+        }
     }
 
     insert(into, fields, values) {
@@ -64,15 +80,19 @@ class DBConnection {
             return;
         }
 
+        var inputTester = /^[A-Za-z0-9*]+$/;
+
         var fieldString = "";
         fields.forEach((i) => {
-            fieldString += i + ", ";
+            if (i.match(inputTester))
+                fieldString += i + ", ";
         });
         fieldString = fieldString.substring(0, fieldString.length - 2);
 
         var valuesString = "";
         values.forEach((i) => {
-            valuesString += "'" + i + "', ";
+            if (i.match(inputTester))
+                valuesString += "'" + i + "', ";
         });
         valuesString = valuesString.substring(0, valuesString.length - 2);
         return this.queryDB("INSERT INTO " + into + " (" + fieldString + ") VALUES (" + valuesString + ");");
