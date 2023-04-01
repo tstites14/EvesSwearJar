0ae5dc1658c1c0d24646286d570f2d4e5fba8e30
Theodore Stites <argionad@gmail.com>
2023-04-01 19:48:35 -0400

    Add random command

--- commands/random.js
+++ commands/random.js
@@ -3,8 +3,26 @@ const Command = require('./command.js');
 class Random extends Command {
     run(dbConnection) {
         return new Promise((resolve, reject) => {
-            resolve(this.command);
-        });
+            dbConnection.select("COUNT(*) AS 'iCounter'", "swears", "interesting", "1")
+            .then((value) => {
+                var count = parseInt(value[0].iCounter);
+                var rand = Math.floor(Math.random() * count) + 1;
+
+                dbConnection.select("*", "swears", "id", rand.toString())
+                .then((value) => {
+                    var timestamp = value[0].datetime;
+                    var monthName = timestamp.toLocaleString('default', { month: 'long' });
+    
+                    resolve(`"${value[0].phrase}" - Eve ${monthName}, ${timestamp.getFullYear()}`);
+                })
+                .catch((err) => {
+                    reject(err.message);
+                })
+            })
+            .catch((err) => {
+                reject(err.message);
+            });
+        })
     }
 }
 
