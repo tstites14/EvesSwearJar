d2bda1e3474da74fce16caeca6858cc31187f006
Theodore Stites <argionad@gmail.com>
2023-04-02 18:32:11 -0400

    Fix for typos in list.js and remove.js

--- commands/list.js
+++ commands/list.js
@@ -5,25 +5,55 @@ class List extends Command {
         return new Promise((resolve, reject) => {
             dbConnection.select("quantity, category", "swears", "DATE_FORMAT(datetime, '%Y-%m-%d')", "CURDATE()", "DESC")
             .then((value) => {
-                var totalCount = value.length;
+                var todayCount = value.length;
     
-                dbConnection.selectGroup("category, COUNT(category) AS catCount", "swears", "category", "DATE_FORMAT(datetime, '%Y-%m-%d')", "CURDATE()", "catCount DESC", true)
-                    .then((value) => {
+                dbConnection.select("COUNT(*) AS 'total'", "swears")
+                .then((value) => {
+                    var allTime = value[0].total;
 
+                    dbConnection.selectGroup("category, COUNT(category) AS catCount", "swears", "category", "DATE_FORMAT(datetime, '%Y-%m-%d')", "CURDATE()", "catCount DESC", true)
+                    .then((value) => {
                         if (value[0] != undefined && value[0] != null) {
-                            resolve(`Eve has cursed ${totalCount} times today. The word of the day is ${value[0].category}!`);
+                            var output = `Eve has cursed ${todayCount} `;
+                            output = this.#formatOutput(output, todayCount, allTime, $value[0].category);
+
+                            resolve(output);
                         } else {
-                            resolve(`Eve has cursed ${totalCount} times today.`);
+                            var output = `Eve has cursed ${todayCount} `;
+                            output = this.#formatOutput(output, todayCount, allTime);
+
+                            resolve(output);
                         }
                     })
                     .catch((err) => {
                         reject(err.message);
                     });
+                }).catch((err) => {
+                    reject(err.message);
+                })
+
             })
             .catch((err) => {
                 reject(err.message);
             });
         });
+    }
+
+    #formatOutput(output, todayCount, allTimeCount, category = null) {
+        if (todayCount > 1)
+            output += `times today. `
+        else
+            output += `time today. `
+        
+        if (allTimeCount > 1)
+            output += `She has cursed ${allTimeCount} times since INSERT FIRST DAY HERE. `
+        else
+            output += `She has cursed ${allTimeCount} time since INSERT FIRST DAY HERE. `
+
+        if (todayCount != 0 && category != null)
+            output += `The word of the day is ${category}`;
+
+        return output;
     }
 }
 
--- commands/remove.js
+++ commands/remove.js
@@ -17,9 +17,9 @@ class Remove extends Command {
                     dbConnection.update("params", ["squats"], [newValue.toString()], "squats", currentValue.toString());
 
                     if (removedSquats == 1)
-                        resolve(`${removedSquats.toString()} squat has been removed from the counter`);
+                        resolve(`${removedSquats.toString()} squat has been removed from the counter. There are still ${newValue} remaining`);
                     else if (removedSquats > 1)
-                        resolve(`${removedSquats.toString()} squats have been removed from the counter`)
+                        resolve(`${removedSquats.toString()} squats have been removed from the counter. There are still ${newValue} remaining`)
                     else reject(new Error("You can't remove a negative number of squats!"))
                 })
                 .catch((err) => {
