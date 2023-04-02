3b8d0db4cde414ee2eaddd63aeb74f0ecd4975e2
Theodore Stites <argionad@gmail.com>
2023-04-02 14:40:14 -0400

    Add remove command

--- commands/remove.js
+++ commands/remove.js
@@ -3,7 +3,23 @@ const Command = require('./command.js');
 class Remove extends Command {
     run(dbConnection) {
         return new Promise((resolve, reject) => {
-            resolve(this.command);
+            var removedSquats = parseInt(this.commandArray[1]);
+
+            dbConnection.select("squats", "params")
+                .then((value) => {
+                    var currentValue = value[0].squats;
+                    var newValue = parseInt(currentValue) - removedSquats;
+
+                    //Ensure that squat values cannot go below 0
+                    if (newValue < 0)
+                        newValue = 0;
+
+                    dbConnection.update("params", ["squats"], [newValue.toString()], "squats", currentValue.toString());
+                    resolve(newValue.toString());
+                })
+                .catch((err) => {
+                    reject(err.message);
+                });
         });
     }
 }
