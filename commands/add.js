36b83b373436980ad1364dcec269655b7b190615
Theodore Stites <argionad@gmail.com>
2023-04-02 13:41:37 -0400

    Add command now also increases the squat counter

--- commands/add.js
+++ commands/add.js
@@ -8,12 +8,25 @@ class Add extends Command {
 
         return new Promise((resolve, reject) => {
             if (uniqueWords.length > 0) {
+                var newSquats = 0;
+
                 for (var i = 0; i < uniqueWords.length; i++) {
                     //dbConnection.insert("swears", ["category", "phrase", "quantity"], [wordArray[i][0], phrase.getPhrase(), wordArray[i][1]]);
+                    newSquats += parseInt(uniqueWords[i][1]);
                 }
-            }
 
-            resolve(this.command);
+                //Add squat counter increase
+                dbConnection.select("squats", "params")
+                .then((value) => {
+                    var currentValue = value[0].squats;
+
+                    dbConnection.update("params", ["squats"], [(parseInt(currentValue) + newSquats).toString()], "squats", currentValue.toString());
+                    resolve((parseInt(currentValue) + newSquats).toString());
+                })
+                .catch((err) => {
+                    reject(err.message);
+                });
+            }
         });
     }
 }
