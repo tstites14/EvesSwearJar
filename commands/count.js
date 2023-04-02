441ba1e5528978522677cefa1594ed060b9584ea
Theodore Stites <argionad@gmail.com>
2023-04-02 15:47:07 -0400

    Add count function

--- commands/count.js
+++ commands/count.js
@@ -3,7 +3,17 @@ const Command = require('./command.js');
 class Count extends Command {
     run(dbConnection) {
         return new Promise((resolve, reject) => {
-            resolve(this.command);
+            var category = this.commandArray[1];
+
+            dbConnection.select("COUNT(*) as 'wordCount'", "swears", "category", category)
+            .then((value) => {
+                var count = parseInt(value[0].wordCount);
+
+                resolve(`Eve has said ${category} ${count} times!`);
+            })
+            .catch((err) => {
+                reject(err.message)
+            })
         });
     }
 }
