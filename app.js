831e2f2929394656afc3500d57f52affc7d4d582
Theodore Stites <argionad@gmail.com>
2023-04-02 12:28:17 -0400

    Remove unnecessary dependency from app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-const DBConnection = require('./dbconnection.js');
 const Phrase = require('./phrase.js');
 
 const CommandManager = require('./commands/commandmanager.js');
@@ -8,7 +7,6 @@ var app = express();
 app.get('/evesswearjar', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    var dbConnection = new DBConnection();
     var phrase = new Phrase(req.query.phrase);
 
     /*Command code starts here*/
