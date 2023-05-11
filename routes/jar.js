ded5fc80831faa4464bf1fb23a1acd8caf110ca3
Theodore Stites <argionad@gmail.com>
2023-05-11 03:02:00 -0400

    Set up routes for svg and png jar images

--- routes/jar.js
+++ routes/jar.js
@@ -5,12 +5,18 @@ const DBConnection = require('../dbconnection.js');
 
 router.get('/jar', function(req, res) {
     res.sendFile('jar.html', { root: './public/TheJar' });
-    res.sendFile('jar.png', { root: './public/TheJar' });
-    res.sendFile('jar.svg', { root: './public/TheJar' });
 })
 
 router.get('/jar/jar.js', function(req, res) {
     res.sendFile('jar.js', { root: './public/TheJar' });
+})
+
+router.get('/jar/jar.png', function(req, res) {
+    res.sendFile("jar.png", { root: './public/TheJar' });
+})
+
+router.get('/jar/jar.svg', function(req, res) {
+    res.sendFile("jar.svg", { root: './public/TheJar' });
 })
 
 router.get("/jar/status", function(req, res) {
