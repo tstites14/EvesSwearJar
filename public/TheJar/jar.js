0b1cdfb4e72c257e541d568d2e2595dd9802a02e
Theodore Stites <argionad@gmail.com>
2023-05-16 23:17:00 -0400

    Add beta version of stream jar

--- public/TheJar/jar.js
+++ public/TheJar/jar.js
@@ -1,9 +1,16 @@
 /*Matter.js jar*/
 const Engine = Matter.Engine,
+    Common = Matter.Common,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+    Body = Matter.Body,
+    Composite = Matter.Composite,
+    Svg = Matter.Svg,
+    Vertices = Matter.Vertices;
+
+//Set up decomp for jar outline
+Common.setDecomp(decomp);
 
 // create an engine
 const engine = Engine.create();
@@ -11,16 +18,61 @@ const engine = Engine.create();
 // create a renderer
 const render = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+    options: {
+        width: 800,
+        height: 800,
+        wireframes: false,
+        showAngleIndicator: false,
+        background: 'transparent'
+    }
 });
 
-// create two boxes and a ground
-const boxA = Bodies.rectangle(400, 200, 80, 80);
-const boxB = Bodies.rectangle(450, 50, 80, 80);
-const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+//Load jar outline SVG
+function select(root, selector) {
+    return Array.prototype.slice.call(root.querySelectorAll(selector));
+}
 
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+function loadSVG(url) {
+    return fetch(url)
+        .then((response) => { return response.text() })
+        .then((raw) => { return new window.DOMParser().parseFromString(raw, 'image/svg+xml') })
+}
+
+loadSVG("http://wh1178760.ispot.cc/evesswearjar/jar/jar.svg")
+    .then((root) => {
+        const paths = select(root, 'path');
+        const vertexSets = paths.map(function(path) { return Vertices.scale(Svg.pathToVertices(path, 15), 3, 3); });
+        
+        const jar = Bodies.fromVertices(420, 510, vertexSets, {
+            isStatic: true,
+            render: {
+                strokeStyle: '#ffffff00',
+                fillStyle: '#ffffff00',
+                lineWidth: 0
+            }
+        });
+
+        const spriteContainer = Bodies.rectangle(400, 400, 300, 300, {
+            isStatic: true,
+            isSensor: true,
+            render: {
+                sprite: {
+                    texture: 'http://wh1178760.ispot.cc/evesswearjar/jar/jar.png',
+                    xoffset: 0,
+                    xScale: 1,
+                    yOffset: 0,
+                    yScale: 1
+                },
+                lineWidth: 0
+            },
+            collisionFilter: {
+                mask: 0
+            }
+        });
+        
+        Composite.add(engine.world, [spriteContainer, jar]);
+    });
 
 // run the renderer
 Render.run(render);
@@ -47,8 +99,11 @@ function checkStatus() {
     getStatus().then(value => {
         console.log(value);
         
-        if (value == "1")
-            Composite.add(engine.world, [Bodies.circle(400, 200, 80)]);
+        if (value == "1") {
+            const coin = Bodies.circle(400, 200, 30);
+
+            Composite.add(engine.world, coin);
+        }
     });
 }
 
